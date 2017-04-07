b234a9bf606d9643b1b11d02f1180aba395fd714
Aviram (Duke) Moran <aviram.moran@gmail.com>
2017-04-07 14:39:43 -0500

    Working GPIB intigration

---
+++ PictureMapping/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace PictureMapping
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.volts = new System.Windows.Forms.ComboBox();
+            this.current = new System.Windows.Forms.ComboBox();
+            this.voltDisplay = new System.Windows.Forms.RichTextBox();
+            this.currentDisplay = new System.Windows.Forms.RichTextBox();
+            this.OnDisplay = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PictureMapping.Properties.Resources.FP;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(584, 183);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.DoubleClick += new System.EventHandler(this.pictureBox1_DoubleClick);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 202);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(583, 21);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "Status";
+            // 
+            // volts
+            // 
+            this.volts.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.volts.FormattingEnabled = true;
+            this.volts.Items.AddRange(new object[] {
+            "1",
+            "1.5",
+            "2",
+            "2.5",
+            "3",
+            "3.5",
+            "4",
+            "4.5",
+            "5",
+            "5.5"});
+            this.volts.Location = new System.Drawing.Point(286, 118);
+            this.volts.Name = "volts";
+            this.volts.Size = new System.Drawing.Size(41, 21);
+            this.volts.TabIndex = 2;
+            this.volts.Visible = false;
+            this.volts.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // current
+            // 
+            this.current.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.current.FormattingEnabled = true;
+            this.current.Items.AddRange(new object[] {
+            ".1",
+            ".2",
+            ".25",
+            ".5",
+            ".75",
+            "1",
+            "1.25",
+            "1.5",
+            "1.75",
+            "2"});
+            this.current.Location = new System.Drawing.Point(288, 154);
+            this.current.Name = "current";
+            this.current.Size = new System.Drawing.Size(41, 21);
+            this.current.TabIndex = 3;
+            this.current.Visible = false;
+            this.current.SelectedIndexChanged += new System.EventHandler(this.current_SelectedIndexChanged);
+            // 
+            // voltDisplay
+            // 
+            this.voltDisplay.BackColor = System.Drawing.Color.Black;
+            this.voltDisplay.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.voltDisplay.Font = new System.Drawing.Font("DS-Digital", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.voltDisplay.ForeColor = System.Drawing.Color.Turquoise;
+            this.voltDisplay.Location = new System.Drawing.Point(132, 38);
+            this.voltDisplay.Name = "voltDisplay";
+            this.voltDisplay.ReadOnly = true;
+            this.voltDisplay.Size = new System.Drawing.Size(121, 42);
+            this.voltDisplay.TabIndex = 4;
+            this.voltDisplay.Text = "";
+            // 
+            // currentDisplay
+            // 
+            this.currentDisplay.BackColor = System.Drawing.Color.Black;
+            this.currentDisplay.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.currentDisplay.Font = new System.Drawing.Font("DS-Digital", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.currentDisplay.ForeColor = System.Drawing.Color.Turquoise;
+            this.currentDisplay.Location = new System.Drawing.Point(259, 38);
+            this.currentDisplay.Name = "currentDisplay";
+            this.currentDisplay.ReadOnly = true;
+            this.currentDisplay.Size = new System.Drawing.Size(118, 42);
+            this.currentDisplay.TabIndex = 5;
+            this.currentDisplay.Text = "";
+            // 
+            // OnDisplay
+            // 
+            this.OnDisplay.BackColor = System.Drawing.Color.Black;
+            this.OnDisplay.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.OnDisplay.Font = new System.Drawing.Font("DS-Digital", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OnDisplay.ForeColor = System.Drawing.Color.Turquoise;
+            this.OnDisplay.Location = new System.Drawing.Point(383, 38);
+            this.OnDisplay.Name = "OnDisplay";
+            this.OnDisplay.ReadOnly = true;
+            this.OnDisplay.Size = new System.Drawing.Size(88, 42);
+            this.OnDisplay.TabIndex = 6;
+            this.OnDisplay.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 227);
+            this.Controls.Add(this.OnDisplay);
+            this.Controls.Add(this.currentDisplay);
+            this.Controls.Add(this.voltDisplay);
+            this.Controls.Add(this.current);
+            this.Controls.Add(this.volts);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Agilent 66309D Controller";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ComboBox volts;
+        private System.Windows.Forms.ComboBox current;
+        private System.Windows.Forms.RichTextBox voltDisplay;
+        private System.Windows.Forms.RichTextBox currentDisplay;
+        private System.Windows.Forms.RichTextBox OnDisplay;
+    }
+}
+
---
+++ PictureMapping/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Ivi.Visa.Interop;
+
+namespace PictureMapping
+{
+
+    public partial class Form1 : Form
+    {
+        private readonly ResourceManager _rm = new ResourceManager();
+        private readonly FormattedIO488 _io488Con = new FormattedIO488();
+        int xCoordinate;
+        int yCoordinate;
+        string volteSet;
+        string currentSet;
+        bool DisplayOn = false;
+        bool OutputOn = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            xCoordinate = e.X;
+            yCoordinate = e.Y;
+            if (e.X > 13 && e.X < 43 && e.Y > 120 && e.Y < 140)
+                this.Cursor = Cursors.Hand; //Power on Key
+            else if (e.X > 60 && e.X < 100 && e.Y > 105 && e.Y < 128)
+                this.Cursor = Cursors.Hand; 
+            else if (e.X > 330 && e.X < 368 && e.Y > 142 && e.Y < 168)
+                this.Cursor = Cursors.Hand; // Output On / off Toggle
+            else if (e.X > 275 && e.X < 315 && e.Y > 105 && e.Y < 128)
+                this.Cursor = Cursors.Hand; //Voltage Setup
+            else if (e.X > 275 && e.X < 315 && e.Y > 142 && e.Y < 168)
+                this.Cursor = Cursors.Hand; //Current setup
+            else
+                this.Cursor = Cursors.Default;
+        }
+
+        private void pictureBox1_DoubleClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (xCoordinate > 13 && xCoordinate < 43 && yCoordinate > 120 && yCoordinate < 140)
+                this.Close();
+            else if (xCoordinate > 60 && xCoordinate < 100 && yCoordinate > 105 && yCoordinate < 128)
+            {
+                if (DisplayOn)
+
+                {
+                    DisplayOn = false;
+                    WriteToGPIB(":DISPLAY ON;", "5");
+                    OnDisplay.Text = "";
+                    voltDisplay.Text = "";
+                    currentDisplay.Text = "";
+                }
+                else if (!DisplayOn)
+                {
+                    DisplayOn = true;
+                    WriteToGPIB(":DISPLAY OFF;", "5");
+                    OnDisplay.Text = "Display OFF";
+                    voltDisplay.Text = volteSet +" V";
+                    currentDisplay.Text = currentSet + " A";
+                }
+            }
+            else if (xCoordinate > 330 && xCoordinate < 368 && yCoordinate > 142 && yCoordinate < 168)
+            { //Output Power On/Off Toggle
+                if (OutputOn)
+                {
+                    OutputOn = false;
+                    WriteToGPIB(":OUTP OFF;", "5");
+                    OnDisplay.Text = "Output OFF";
+                    voltDisplay.Text = volteSet + "V";
+                    currentDisplay.Text = currentSet + "A";
+                }
+                else if (!OutputOn)
+                {
+                    OutputOn = true;
+                    WriteToGPIB(":OUTP ON;", "5");
+                    OnDisplay.Text = "Output ON";
+                    voltDisplay.Text = volteSet + " V";
+                    currentDisplay.Text = currentSet + " A";
+                }
+            }
+            else if (xCoordinate > 275 && xCoordinate < 315 && yCoordinate > 105 && yCoordinate < 128)
+            {
+                volts.Visible = true;
+            }
+            else if (xCoordinate > 275 && xCoordinate < 315 && yCoordinate > 142 && yCoordinate < 168)
+            {
+                current.Visible = true;
+            }
+        }
+
+        private void WriteToGPIB(string GPIBCommand, string GPIBAddress)
+            //when this method is called it must include the command and GPIB adrress of the device.
+        {
+            var resourceString = ("GPIB0::" + GPIBAddress + "::INSTR");
+            try
+            {
+                _io488Con.IO = (IMessage)_rm.Open(resourceString, AccessMode.NO_LOCK, 0);
+                _io488Con.IO.Clear();
+                _io488Con.WriteString(GPIBCommand);
+                Housecleaning();
+                richTextBox1.BackColor = Color.LimeGreen;
+                richTextBox1.Text = @"Command Execution Completed";
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                _io488Con.IO.Close();
+            }
+        }
+
+        private string WriteThenReadFromGPIB(string GPIBCommand, string GPIBAddress)
+        {
+            string GPIBRead = "";
+            var resourceString = ("GPIB0::" + GPIBAddress +"::INSTR");
+            try
+            {
+                _io488Con.IO = (IMessage)_rm.Open(resourceString, AccessMode.NO_LOCK, 0);
+                _io488Con.IO.Clear();
+                _io488Con.WriteString(GPIBCommand);
+                {
+                    GPIBRead = _io488Con.ReadString();
+                    Housecleaning();
+                    richTextBox1.BackColor = Color.LimeGreen;
+                    richTextBox1.Text = @"Command Execution Completed";
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                _io488Con.IO.Close();
+            }
+            return GPIBRead;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            volteSet = volts.Text;
+            WriteToGPIB(":VOLT " + volteSet + ".1;", "5");
+            Housecleaning();
+            richTextBox1.Text = ("Voltage Set To " + volteSet);
+            volts.Visible = false;
+        }
+
+        public void Housecleaning()
+        {
+            richTextBox1.ResetBackColor();
+            richTextBox1.Clear();
+        }
+
+        private void current_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            currentSet = current.Text;
+            WriteToGPIB(":CURR " + currentSet + ";", "5");
+            Housecleaning();
+            richTextBox1.Text = ("Current Set To " + volteSet);
+            current.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Housecleaning();
+            WriteToGPIB("*RST", "5");
+            WriteToGPIB("*CLS", "5");
+            WriteToGPIB("STAT:PRES", "5");
+            WriteToGPIB("*ESE 0", "5");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
